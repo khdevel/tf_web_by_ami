7c2da7a522a2f6fdf63014f3ecf3a598131729ef
kamil.herbik <kamil.herbikl@dataio.pl>
2017-06-20 17:07:03 +0200

    extended security group

--- securitygroups.tf
+++ securitygroups.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "allow_web" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 443
