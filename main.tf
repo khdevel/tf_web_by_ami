9047c3f2da601862cf30044325df4bd29c00e64d
kherbik <kamil.herbik@rst.com.pl>
2017-06-21 15:15:12 +0200

    separated module for webhost with securitygroup

--- main.tf
+++ main.tf
@@ -2,17 +2,15 @@ provider "aws" {
   region = "${var.region}"
 }
 
-data "aws_ami_ids" "awslinux" {
-  owners = ["self"]
-
-  filter {
-    name   = "name"
-    values = ["${var.ami_filter}"]
-  }
-}
-
 module "remote_state" {
   source      = "git@github.com:khdevel/tf_remote_state.git"
   prefix      = "${var.prefix}"
   environment = "${var.environment}"
+}
+
+module "webhost" {
+  source        = "./module"
+  key_name      = "${var.key_name}"
+  ami_filter    = "${var.ami_filter}"
+  instance_type = "${var.instance_type}"
 }
